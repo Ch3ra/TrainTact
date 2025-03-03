a0ad6906ab8de24c605646ace0b2a96945ecfabb
Alish Ban <lishwas23.@gmail.com>
2025-03-03 11:06:50 +0545

    simple Css improve in the User Profile

--- Backend/middleware/multerConfig.js
+++ Backend/middleware/multerConfig.js
@@ -3,20 +3,20 @@ const path = require("path");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    let uploadFolder = "./uploads"; // Default folder if none matches
+    let uploadFolder = "./uploads";
 
     if (file.fieldname === "profilePicture") {
       uploadFolder = "./uploads/profilePictures";
     } else if (file.fieldname === "resume") {
       uploadFolder = "./uploads/resumes";
     } else if (file.fieldname === "coverPhoto") {
-      uploadFolder = "./uploads/coverPhoto"; // Handling uploads to the coverPhoto folder
+      uploadFolder = "./uploads/coverPhoto";
     }
 
-    cb(null, uploadFolder); // Set the destination folder
+    cb(null, uploadFolder); 
   },
   filename: function (req, file, cb) {
-    // Use a unique filename with a timestamp to prevent conflicts
+    
     const uniqueName = Date.now() + "-" + file.originalname.replace(/\s+/g, "-");
     cb(null, uniqueName);
   },
